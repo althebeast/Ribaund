73509b4b02d6a28f6bf38f0ad345e0db7ae076b6
Alperen Sarışan <88689564+althebeast@users.noreply.github.com>
2025-10-28 16:34:24 -0400

    Initial Commit

---
+++ Ribaund/RibaundApp.swift
@@ -0,0 +1,17 @@
+//
+//  RibaundApp.swift
+//  Ribaund
+//
+//  Created by Alperen Sarisan on 28/10/25.
+//
+
+import SwiftUI
+
+@main
+struct RibaundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
